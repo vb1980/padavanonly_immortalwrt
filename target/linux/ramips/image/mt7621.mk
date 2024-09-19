20c5fb1ac6b5665e9c2a43bd139dd86261572cd5
vb1980 <vb1980@sina.com>
2024-09-19 21:16:17 +0800

    update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -311,7 +311,7 @@ define Device/c-life_xg1
   PAGESIZE := 2048
   UBINIZE_OPTS := -E 5
   KERNEL_SIZE := 4096k
-  IMAGE_SIZE := 128512k
+  IMAGE_SIZE := 206848k
   IMAGES += factory.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \

c0a8795c4c6094dd868660a472f7215b2dff7ac0
vb1980 <vb1980@sina.com>
2024-09-20 13:18:42 +0800

    update mt7621_c-life_xg1.dts and mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -307,11 +307,7 @@ TARGET_DEVICES += bolt_arion
 define Device/c-life_xg1
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  UBINIZE_OPTS := -E 5
-  KERNEL_SIZE := 4096k
-  IMAGE_SIZE := 206848k
+  IMAGE_SIZE := 91136k
   IMAGES += factory.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \

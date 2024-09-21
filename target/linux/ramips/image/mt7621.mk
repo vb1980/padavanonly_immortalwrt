504a1119134431cc6ed655f0ab89e3306782bda3
vb1980 <vb1980@sina.com>
2024-09-21 20:50:24 +0800

    try to fix build C-Life XG1 issue

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -306,12 +306,15 @@ TARGET_DEVICES += bolt_arion
 
 define Device/c-life_xg1
   $(Device/dsa-migration)
-  $(Device/uimage-lzma-loader)
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  UBINIZE_OPTS := -E 5
+  KERNEL_SIZE := 4096k
   IMAGE_SIZE := 91136k
   IMAGES += factory.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
-	  check-size
+	check-size
   DEVICE_VENDOR := C-Life
   DEVICE_MODEL := XG1
   DEVICE_PACKAGES := luci-app-mtwifi uboot-envtools kmod-usb3
